fa16286731fecac7f0b938de14d7776cdabb0d22
swarnabha2002 <swaarnabharoychowdhury@gmail.com>
2020-09-15 20:28:49 +0530

    first commit

---
+++ easybank.js
@@ -0,0 +1,10 @@
+function onclickChange(){
+       
+    document.getElementById("hambtn").classList.toggle("header__menu");
+    document.getElementById("poverlay").classList.toggle("overlay");
+    document.getElementById("body2").classList.toggle("bodyclass");
+
+
+};
+
+
